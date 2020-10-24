78fa1b8032c559f211919e1b913803b9a7626dcb
luonghv-31 <63768512+luonghv-31@users.noreply.github.com>
2020-10-24 16:16:12 +0700

    Add files via upload

---
+++ src/main/java/mydictionary/Dictionary.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mydictionary;
+import java.util.ArrayList;
+/**
+ *
+ * @author TCC
+ */
+public class Dictionary {
+    public static ArrayList<Mydictionary> dat = new ArrayList<>();
+}
